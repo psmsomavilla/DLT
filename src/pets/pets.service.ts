ba7ff4f1ddc42e67004e08611048e824b9dfbef4
pablo somavilla martin <psm.somavilla@gmail.com>
2026-03-03 17:50:27 +0100

    feat (pets.service. vinculacion de entidades cat-user a través de pet)

--- src/pets/pets.service.ts
+++ src/pets/pets.service.ts
@@ -1,26 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import {Injectable, NotFoundException} from '@nestjs/common';
 import { CreatePetDto } from './dto/create-pet.dto';
 import { UpdatePetDto } from './dto/update-pet.dto';
+import {InjectRepository} from "@nestjs/typeorm";
+import {Cat} from "../cats/entities/cat.entity";
+import {Pet} from "./entities/pet.entity";
+import {Repository} from "typeorm";
 
 @Injectable()
 export class PetsService {
-  create(createPetDto: CreatePetDto) {
-    return 'This action adds a new pet';
-  }
+  @InjectRepository(Pet)
+  private readonly petsRepository: Repository<Pet>;
+
+  @InjectRepository(Cat)
+  private readonly catRepository: Repository<Cat>;
+
+
 
   findAll() {
-    return `This action returns all pets`;
+    return this.petsRepository.find({
+      relations: ["user","cat"],
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pet`;
-  }
 
-  update(id: number, updatePetDto: UpdatePetDto) {
-    return `This action updates a #${id} pet`;
-  }
+  async createPet(userId: number, catId: number,  petName:string) {
+    // buscamos el gato en la bd por id
+    const cat = await this.catRepository.findOneBy({id: catId});
 
-  remove(id: number) {
-    return `This action removes a #${id} pet`;
+    if (!cat) {
+      throw new NotFoundException("El gato con esa ID no existe");
+    }
+
+    const newPet = this.petsRepository.create({
+      name: petName,
+      user: {id: userId}, // relacion con el dueño
+      cat:cat,            // relacion con la entidad cat
+    });
+    return await this.petsRepository.save(newPet);
+
+
+
+
   }
 }
