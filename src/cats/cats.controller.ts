ee34617289af16c725d7b79c0def907b15607d01
psmsomavilla <psm.somavilla@gmail.com>
2026-03-11 16:16:19 +0100

    coañadido en cats controller Guards para dar prioridad solo al admin en sus acciones específicas

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,10 +1,14 @@
-import {Controller, Delete, Get, Param, Post, Query} from '@nestjs/common';
+import {Controller, Delete, Get, Param, Post, Query, UseGuards} from '@nestjs/common';
 import {CatsService} from './cats.service';
-import {ApiTags} from "@nestjs/swagger";
+import {ApiBearerAuth, ApiTags} from "@nestjs/swagger";
 import {UserRole} from "../users/entities/user.entity";
 import {Roles} from "../auth/decorators/roles.decorator";
+import {RolesGuard} from "../auth/guards/roles.guard";
+import {AuthGuard} from "@nestjs/passport";
 
 @ApiTags("cat - Módulo para gestionar datos de gatos")
+@ApiBearerAuth()
+@UseGuards(AuthGuard,RolesGuard)
 @Controller('cat')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
