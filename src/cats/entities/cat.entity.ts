87c7214cb4c1b5e1debff0e2a287e11236438948
psmsomavilla <psm.somavilla@gmail.com>
2026-03-11 10:51:53 +0100

    creacion entidad cat con sus atributos

--- src/cats/entities/cat.entity.ts
+++ src/cats/entities/cat.entity.ts
@@ -1,1 +1,33 @@
-export class Cat {}
+import {Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+
+@Entity()
+export class Cat {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    externalId: number;
+
+    @Column()
+    url: string;
+
+    @Column()
+    width:number;
+
+    @Column()
+    height:number;
+
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    @DeleteDateColumn()
+    deleted_at: Date;
+
+
+
+}
+
