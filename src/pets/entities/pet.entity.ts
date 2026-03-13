2865b26d3052fb8ae2e0f474349367d7347d4322
psmsomavilla <psm.somavilla@gmail.com>
2026-03-13 10:01:53 +0100

    creacion de la entidad pet con sus atributos

--- src/pets/entities/pet.entity.ts
+++ src/pets/entities/pet.entity.ts
@@ -1,1 +1,26 @@
-export class Pet {}
+import {Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
+
+@Entity()
+export class Pet {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    petName: string;
+
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    @DeleteDateColumn()
+    deleted_at: Date;
+
+
+
+
+
+
+}
